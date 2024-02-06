c08dd9544f08c00e3ef9b85ca873159c098da98d
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-06 18:00:41 +0530

    Pgm

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolApplicationCrud.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ClassesDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ClassesDbContext") ?? throw new InvalidOperationException("Connection string 'ClassesDbContext' not found.")));
+builder.Services.AddDbContext<SubjectDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SubjectDbContext") ?? throw new InvalidOperationException("Connection string 'SubjectDbContext' not found.")));
+builder.Services.AddDbContext<SchoolDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDbContext") ?? throw new InvalidOperationException("Connection string 'SchoolDbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
